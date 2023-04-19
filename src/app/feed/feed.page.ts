4298804ee79f8e5a1a6971f390e0648368925e33
Nishantec <guptanishant1307@gmail.com>
2023-04-20 03:40:16 +0530

    added orders active & new block

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
+
+  orders: any = {
+    active: [
+      {
+        orderId: 123,
+        pickupAddress: {
+          restaurantName: 'Pizza Palace',
+          address: '123 Main St'
+        }
+      },
+      {
+        orderId: 456,
+        pickupAddress: {
+          restaurantName: 'Burger Joint',
+          address: '456 Oak Ave'
+        }
+      }
+    ],
+    new: [
+      {
+        orderId: 789,
+        pickupAddress: {
+          restaurantName: 'Taco Town',
+          address: '789 Maple St'
+        }
+      }
+    ]
+  };
+
+  showActiveOrders = true;
 constructor(){}
 
   ngOnInit() {

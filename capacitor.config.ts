9be2374832240dcb553306f23f641698dcfce67d
bleeding-edge-dev <work.bleedingedge@gmail.com>
2023-07-04 15:19:46 +0530

    android-studio changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'OnestopRider',
-  webDir: 'www',
+  appId: "com.onestopdelivery.rider",
+  appName: "OnestopRider",
+  webDir: "www",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
